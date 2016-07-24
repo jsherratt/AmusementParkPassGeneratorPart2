058ca573a6b4bd0ef762af406c292c682eb27598
jsherratt <joe.sherratt1@gmail.com>
2016-07-24 22:15:41 +0100

    Initial Commit

---
+++ Amusment Park Pass Generator: Part 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amusment Park Pass Generator: Part 2
+//
+//  Created by Joe Sherratt on 24/07/2016.
+//  Copyright © 2016 jsherratt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
